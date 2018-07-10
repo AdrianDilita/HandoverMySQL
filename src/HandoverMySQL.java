2f9bae7a001de7a403418b9e3dd93a03d722dba7
AdrianDilita <adrian.dilita@mindit.io>
2018-07-10 17:24:04 +0300

    Added manual stop condition - type 'end'

--- src/HandoverMySQL.java
+++ src/HandoverMySQL.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
+import org.apache.commons.lang.StringUtils;
 
 public class HandoverMySQL {
     public static void main(String args[]) {
         String newLine = System.getProperty("line.separator"); //Define new line for the welcome message
         System.out.print("Please enter your SQL code..." + newLine);
         Scanner userinput = new Scanner(System.in);
+
+        String emptyspace = "";
         while (userinput.hasNextLine()) {
-            String tokens = userinput.nextLine();
-            String nolockremove = tokens.replaceFirst ("with \\(nolock\\)|(nolock)|\\(NOLOCK\\)|(NOLOCK)","");
-            String commentblockadd = nolockremove.replaceFirst(" ", System.lineSeparator() + "                        updated for v0.06                " + System.lineSeparator());
-            System.out.println(newLine + commentblockadd);
+            String line = userinput.nextLine();
+            if("end".equals(line)) { //Temporary solution to stop application
+                break;
+            }
+            emptyspace += "\n" + userinput.nextLine();
+
         }
         userinput.close();
+        String replaced = StringUtils.replaceOnce (emptyspace, " ", "/*                        updated for v0.06                */");
+        System.out.println(replaced);
     }
 }

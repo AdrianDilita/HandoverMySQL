efa697949c234b43cffc02faef8d888f49dc0ba9
unknown <adrian.dilita@mindit.io>
2018-07-10 09:53:48 +0300

    Commit all files

---
+++ src/HandoverMySQL.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class HandoverMySQL {
+    public static void main(String args[]) {
+        String newLine = System.getProperty("line.separator"); //Define new line for the welcome message
+        System.out.print("Please enter your SQL code..." + newLine);
+        Scanner userinput = new Scanner(System.in);
+        String newstring = userinput.nextLine();
+        String nolockremove = newstring.replaceAll("with \\(nolock\\)|(nolock)}", "");
+        String commentblockadd = nolockremove.replaceFirst(" ", System.lineSeparator() + " /*                                           updated for v0.06               */ " + System.lineSeparator());
+        while (userinput.hasNextLine()) {
+            System.out.println(userinput.nextLine()); //comment
+        }
+        System.out.println(commentblockadd);
+    }
+}
+
